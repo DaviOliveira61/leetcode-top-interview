a73034b3b3a0b3a221db6187ab800f9d2c1a9977
Davi Batista <davigbso@hotmail.com>
2025-11-03 19:26:28 -0300

    Remove duplicates from sorted array 2

--- remove-duplicates-part-two/Program.cs
+++ remove-duplicates-part-two/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] numbers = [1, 1, 1, 2, 2, 3];
+
+int k = RemoveDuplicates(numbers);
+
+Console.WriteLine(k);
+
+int RemoveDuplicates(int[] nums)
+{
+    int count = 0, current = 1;
+
+    for (int i = 1; i < nums.Length; i++)
+    {
+        if (nums[i] != nums[i - 1])
+        {
+            count = 0;
+            nums[current++] = nums[i];
+        }
+        else
+        {
+            count++;
+            if (count <= 1)
+            {
+                nums[current++] = nums[i];
+            }
+        }
+    }
+    return current;
+}
